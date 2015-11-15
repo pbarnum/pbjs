770b20ce5865b8fac9bd19f88e78d7efa899bf4c
Patrick Barnum <patrickdbarnum@gmail.com>
2015-11-15 04:50:06 -0800

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+ 
+var paths = {
+  js: ['lib/js/**/*.js'],
+  sass: ['lib/scss/**/*.scss']
+};
+
+// combine source files into one
+gulp.task('combine-js', function() {
+  return gulp.src(paths.js)
+    .pipe(concat('pbjs.js'))
+    .pipe(gulp.dest('src/js'));
+});
+
+// combine source files into one
+gulp.task('combine-sass', function() {
+  return gulp.src(paths.sass)
+    .pipe(sass())
+    .pipe(gulp.dest('src/css'));
+});
+
+// minify source files into one
+gulp.task('compile-js', function() {
+  return gulp.src(paths.js)
+    .pipe(uglify())
+    .pipe(concat('pbjs.min.js'))
+    .pipe(gulp.dest('src/js/minified'));
+});
+
+gulp.task('compile-sass', function () {
+  return gulp.src(paths.sass)
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(rename({
+      "suffix": ".min"
+    }))
+    .pipe(gulp.dest('src/css/minified'));
+});
+
+// Rerun the task when a file changes
+gulp.task('watch', function() {
+  gulp.watch([
+    paths.js,
+    paths.sass
+  ], [
+    'combine-js',
+    'combine-sass'
+  ]);
+});
+
+// The default task (called when you run `gulp` from cli)
+gulp.task('watch', [
+  //'watch',
+  'combine-js',
+  'combine-sass',
+  'compile-js',
+  'compile-sass'
+]);
